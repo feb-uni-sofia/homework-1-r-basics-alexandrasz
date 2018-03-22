1268353d10e4feea19a1af75873d593767b75b36
Alexandra Zaneva <alexandrazaneva@abv.bg>
2018-03-22 23:23:07 +0200

    Homework 1 - problem 1 and problem 2

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,12 @@
-
+x <- c(4,1,1,4)
+y <- c(1,4)
+x-y
+s <- c(x,y)
+rep(s,10)
+srep <- rep(s,10)
+length(srep)
+rep(s, each=3)
+seq(7,21, by=1)
+7:21
+lseq <- seq(7,21, by=1)
+length(lseq)
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,20 @@
-
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+dff <-  xmax - xmin
+avgMAX <- mean(xmax)
+avgMIN <- mean(xmin)
+xmin [xmin < avgMIN]
+xmin [xmax > avgMAX]
+names(xmin) <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri' , 'Sat' , 'Sun')
+names(xmax) <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri' , 'Sat' , 'Sun')
+temperature <- data.frame(xmin,xmax)
+temperature <- within(temperature,{xminFahrenheit <- xmin*9/5 + 32})
+ftemp <- data.frame(
+  Max_temp_Fahr = xmax*9/5 + 32 , 
+  Min_temp_Fahr = xmin*9/5 + 32)
+FivedaytempFahrenheit  <- data.frame(
+  Max_temp_Fahr = xmax[seq(1:5)]*9/5+32,
+  Min_temp_Fahr = xmin[seq(1:5)]*9/5+32)
+	FivedaytempFahrenheit <- data.frame(
+  Max_temp_Fahr = xmax[seq(length(xmax)-2)]*9/5+32 ,
+  Min_temp_Fahr = xmin[seq(length(xmin)-2)]*9/5+32)
